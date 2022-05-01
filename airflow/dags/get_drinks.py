99fb4fca6af86b8a4ad4418a08eb4687d27a395d
joelitam2021 <joel.jaramillo@itam.mx>
2022-05-01 17:58:03 -0500

    add comment into the code

--- airflow/dags/get_drinks.py
+++ airflow/dags/get_drinks.py
@@ -1,3 +1,4 @@
+# General Imports
 import string
 import requests
 import json
@@ -6,7 +7,14 @@ import csv
 from datetime import datetime, timedelta
 
 
+#function that receives a set of drinks with their ingredients and stores them in google cloud as JSON type files
 def write_blob_from_list(list, bucket, path_auth):
+
+#inputs:
+#list: data-frame with the information of the ingredients of each drink
+#bucket: the name of the reserved space to store information in google cloud
+#path_auth: the path of the security file
+    
     client = storage.Client.from_service_account_json(path_auth)
     gcs_bucket = client.get_bucket(bucket)
     for letter in list:
@@ -15,7 +23,14 @@ def write_blob_from_list(list, bucket, path_auth):
         blob.upload_from_string(data=json.dumps(letter[1]), content_type='application/json')
     print(f"Writing {len(letter)} rows to gs://{bucket}/{path}")
 
+
+# Function that connects to the cocktail api and retrieves all the recipes in alphabetical order.    
 def get_drink(bucket, key, path_auth):
+#bucket: the name of the reserved space to store information in google cloud
+#key: reserved code that allows extracting information from the api
+#path_auth: the path of the security file   
+
+#This section generates all the instructions to connect and extract from the api using the key first letter of the name of the drink
     alphabet_string = string.ascii_lowercase
     alphabet_list = list(alphabet_string)  
     cocktails_key = key
@@ -25,11 +40,13 @@ def get_drink(bucket, key, path_auth):
     request_bodies = []
     for letter in alphabet_list:
         request_bodies.append(base_url + json_api + auth + f"/search.php?f={letter}")
-        
+
+#the connection to the api is established and the drinks are extracted in JSON format
     data = []
     for request in request_bodies:
         data.append(requests.get(request).json())
 
+#the relevant information is extracted from the JSON, the labels that are not valid for bigquery are modified
     drink = []
     for letter in data:
         if letter['drinks'] != None: 
@@ -43,5 +60,6 @@ def get_drink(bucket, key, path_auth):
                 del drink_jaison["strInstructionsZH-HANT"]
                 dato[1] = drink_jaison
                 drink.append(dato)
-                
+    
+#the function is commanded to be executed    
     write_blob_from_list(drink, bucket, path_auth)
