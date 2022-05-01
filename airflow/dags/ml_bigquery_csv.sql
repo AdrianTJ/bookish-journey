836c3f05457e5e985cc2958cbd2971f085681171
AdrianTJ <31286933+AdrianTJ@users.noreply.github.com>
2022-05-01 17:22:14 -0500

    Adding a missing SQL file for model training

---
+++ airflow/dags/ml_bigquery_csv.sql
@@ -0,0 +1,31 @@
+BEGIN
+    CREATE TEMP TABLE _SESSION.tmpExportTable AS (
+        SELECT
+            idDrink,
+            strDrink,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+            strIngredient6,
+            strIngredient7,
+            strIngredient8,
+            strIngredient9,
+            strIngredient10,
+            strIngredient11,
+            strIngredient12,
+            strIngredient13,
+            strIngredient14,
+            strIngredient15 
+        FROM `###############`
+    );
+    EXPORT DATA OPTIONS(
+        uri='###############'
+        , format='CSV'
+        , overwrite=true
+        , header=true
+        , field_delimiter=","
+    ) AS
+    SELECT * FROM _SESSION.tmpExportTable;
+END;
